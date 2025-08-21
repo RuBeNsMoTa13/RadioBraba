b8d743c279e22dcc3ea8f946a458dbd739ba593e
Rubens <rubens13mota@gmail.com>
2025-08-21 16:05:42 -0300

    feat(vite.config.ts): add build configuration to optimize chunk sizes

--- vite.config.ts
+++ vite.config.ts
@@ -24,4 +24,16 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  // ADICIONADO: Configuração de build para otimizar o tamanho dos arquivos (chunks)
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 });
